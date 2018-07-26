5e390ceae115126901078424e38b8fd377874f3b
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-07-26 23:48:32 +1200

    day 2 on unity
    add C# to model
    ---------------S1 finished----------------
    S2
    1. assets import

---
+++ Assets/Scenes/S2/S2_Terminal/WM2000/Hacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("hello");
+        Terminal.WriteLine("Hello World");
+        Terminal.WriteLine("Select Level");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Hello World");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
