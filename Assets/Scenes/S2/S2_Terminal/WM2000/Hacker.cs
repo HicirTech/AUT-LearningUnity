d294be8aa169cf3b0254c8ba0712d935f75fcb10
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-07-29 17:04:09 +1200

    day 3 on unity
    1. variables
     1.1 var variable
    2. set up for vs code
     2.1 vscode plugin
     2.2. vscode c# and .net
    3. function
    4. input from user
    5. opeartors and expression
    6. console collapse
    7. if in C#
    8. try and catch block in C#
    9. number veriable
    10. enum in C#
       10.1 use enum to control game stage

--- Assets/Scenes/S2/S2_Terminal/WM2000/Hacker.cs
+++ Assets/Scenes/S2/S2_Terminal/WM2000/Hacker.cs
@@ -1,21 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Hacker : MonoBehaviour {
-
-	// Use this for initialization
+    enum Screen {
+        MainMenu,
+        Passowrd,
+        Win
+    }
+    private int level;
+    Screen currentScreen;
 	void Start () {
-        print("hello");
-        Terminal.WriteLine("Hello World");
-        Terminal.WriteLine("Select Level");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("Hello World");
-
+        this.displayMenu();    
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    private void displayMenu()
+    {
+        this.currentScreen=Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Current Location : [" +this.currentScreen+"]");
+        Terminal.WriteLine("Please select a Level");
+        Terminal.WriteLine("[1] Level 1");
+        Terminal.WriteLine("[2] Level 2");
+        Terminal.WriteLine("[3] Level 3");
+        Terminal.WriteLine("Enter a number for select");
+    }
+       
+
+    private void OnUserInput(string input)
+    {
+        if(input == "menu"||input == "Menu")
+        {
+            this.displayMenu();
+        }
+        else if(this.currentScreen==Screen.MainMenu)
+        {
+            this.inMainMenu(input);
+        }
+        else if(this.currentScreen==Screen.Passowrd)
+        {
+
+        }
+    }
+    private void inMainMenu(String input)
+    {
+        try
+        {
+            this.level=int.Parse(input);
+            gamerStart();
+        }catch(System.FormatException)
+        {
+            Terminal.WriteLine("you are not inputing number");
+            Terminal.WriteLine("Please input again");
+        }
+    }
+    private void gamerStart()
+    {
+        this.currentScreen=Screen.Passowrd;
+        Terminal.WriteLine("Current Location : [" +this.currentScreen+"]");
+        Terminal.WriteLine("you have choosen level: " + this.level);
+        Terminal.WriteLine("Please enter you password");
+    }
+
+      private bool inputCals(string input)
+    {
+    
+        return input=="1"?true:false;
+    }
 }
+//----------------------
+ /* if(input.Length==3&&input=="007")
+       { 
+           print("fuck you");
+           Terminal.WriteLine("fuckyou");
+       }
+       else if(input ==  "menu")
+       {
+           this.printWellcome("Zeting");
+       }
+       else if(input.Length==1)
+       {
+            print("please input useable level");
+            Terminal.WriteLine("please input useable level");
+       }
+       else
+       {
+           Terminal.WriteLine("input error");
+       }*/
+//-----------------------------------------
