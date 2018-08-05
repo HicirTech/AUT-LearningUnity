e2d0b79ba1ca39f691d66d9f5ef156e10df6abe3
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-08-06 09:13:25 +1200

    Day 8 on Unity
    Code refactory
    Level load bug fixed

--- Assets/Scenes/S3/Rocket.cs
+++ Assets/Scenes/S3/Rocket.cs
@@ -2,52 +2,54 @@
 using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
-	 enum State{live,die,wait};
-	AudioSource audioSource;
-	[SerializeField] [Range(0,7)]static int levelCount=0;
-	[SerializeField] bool collisionEnable = true ;
-	[SerializeField] float rcsThrust = 100f;
-	[SerializeField] float mainThrust = 100f;
-	[SerializeField] float levelDelay =2f;
-	[SerializeField] AudioClip engine;
-	[SerializeField] AudioClip death;
-	[SerializeField] AudioClip success;
-	[SerializeField] ParticleSystem running;
-	[SerializeField] ParticleSystem diePart;
-	[SerializeField] ParticleSystem winPart;
 
-	 Rigidbody rigidbody;
+	private AudioSource audioSource;
+	[SerializeField] [Range(0,7)] private static int levelCount=0;
+	[SerializeField] private bool collisionEnable = true ;
+	[SerializeField] private float rcsThrust = 100f;
+	[SerializeField] private float mainThrust = 100f;
+	[SerializeField] private float levelDelay =2f;
+	[SerializeField] private AudioClip engine;
+	[SerializeField] private AudioClip death;
+	[SerializeField] private AudioClip success;
+	[SerializeField] private ParticleSystem running;
+	[SerializeField] private ParticleSystem diePart;
+	[SerializeField] private ParticleSystem winPart;
+	private Rigidbody rigidbody;
+ 	private Vector3 rotate;
+	private Vector3 thrust;
+	private bool isAlive;	
 	
-	 Vector3 rotate;
-	 Vector3 thrust;
-    // Use this for initialization
-
-	  State state;	
-	
-
     void Start () {
-		state=State.live;
+		isAlive=true;
+		this.getReady();
+	}
+
+	void Update () {		
+		if(this.isAlive)
+			this.processInput();	
+	}
+	private void getVetctorReady()
+	{
 		this.rotate = Vector3.forward*(this.rcsThrust*Time.deltaTime);
 		this.thrust=Vector3.up*(this.mainThrust*Time.deltaTime);
-		this.rigidbody=GetComponent<Rigidbody>();
-		this.audioSource = GetComponent<AudioSource>();
-	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		if(this.state==State.live)
-		{
-			this.processInput();	
-		}
+		private void getReady()
+	{
+		this.getVetctorReady();
+		this.getRockReady();
 	}
 	private void processInput()
 	{
 		ResopondToRotate();
 		RespondToThrust();
 		if(Debug.isDebugBuild)
-				debugkey();
+			debugkey();
+	}
+		private void getRockReady()
+	{
+		this.rigidbody=GetComponent<Rigidbody>();
+		this.audioSource = GetComponent<AudioSource>();
 	}
 
 	private void debugkey()
@@ -59,8 +61,7 @@ public class Rocket : MonoBehaviour {
 	{
 		if(Input.GetKey(KeyCode.L))
 		{
-			levelCount++;
-			//audioSource.PlayOneShot(success);
+			//levelCount++;
 			this.LoadLevel();
 		}
 	}
@@ -68,14 +69,13 @@ public class Rocket : MonoBehaviour {
 	{
 		if(Input.GetKey(KeyCode.C))
 		{
-		//	audioSource.PlayOneShot(success);
 			this.collisionEnable=!this.collisionEnable;
 		}
 
 	}
 	private void OnCollisionEnter(Collision collision)
 	{
-		if(state!=State.live)
+		if(isAlive!=true)
 		{
 			return;
 		}
@@ -96,21 +96,15 @@ public class Rocket : MonoBehaviour {
 	}	
 	private	void playWin()
 	{
-		state=State.wait;
-		if(levelCount!=7)
-			levelCount++;
-		else
-			levelCount=0;
 		running.Stop();
 		winPart.Play();
 		audioSource.Stop();
         audioSource.PlayOneShot(success);
 		Invoke("LoadLevel",levelDelay);
-		
 	}
 	private void playDie()
 	{
-		state=State.die;
+		isAlive=!isAlive;
 		running.Stop();
 		diePart.Play();
 		audioSource.Stop();
@@ -119,20 +113,28 @@ public class Rocket : MonoBehaviour {
 	}
 	private void LoadLevel()
 	{
+		if(this.isAlive)
+		{
+			if(levelCount<=7)
+				levelCount++;		
+			else
+				levelCount=0;
+		}
 		SceneManager.LoadScene(levelCount);
 	}
     private void ResopondToRotate()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-			transform.Rotate(rotate);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-			transform.Rotate(-rotate);
-        }
+		this.rigidbody.angularVelocity=Vector3.zero;
+    	this.codeProcess();
 
     }
+	private void codeProcess()
+	{
+		if (Input.GetKey(KeyCode.A))
+			transform.Rotate(rotate);
+        else if (Input.GetKey(KeyCode.D))
+			transform.Rotate(-rotate);
+	}
 
     private void RespondToThrust()
     {
@@ -157,8 +159,10 @@ public class Rocket : MonoBehaviour {
     }
 
 }
-/*
+/* code Rubbish bin
 
+
+	private enum State{live,die,wait};
 	private void LoadLevel0()
 	{
 		SceneManager.LoadScene(0);
