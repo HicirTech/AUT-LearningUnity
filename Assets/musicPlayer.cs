75e5b82ef0b6511316cce5d29f5d6d665a274598
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-08-07 10:00:09 +1200

    S4
    Roll pitch yaw.
    terrain
    models
    partcialsystem

---
+++ Assets/musicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class musicPlayer : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("loadScene", 2f);
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void loadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
