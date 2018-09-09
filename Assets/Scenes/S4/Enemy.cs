fabba879dfd1f5d11b4fdb7835e36d4c3708d04e
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-09 17:03:24 +1200

    enemy added

--- Assets/Scenes/S4/Enemy.cs
+++ Assets/Scenes/S4/Enemy.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-
+	[SerializeField] GameObject deathFX;
+	[SerializeField] Transform  aferdied;
+	private void Start()
+	{
+		Collider enemyCollider  = gameObject.AddComponent<SphereCollider>();
+		enemyCollider.isTrigger=false;		
+	}
 	private void OnParticleCollision(GameObject other)
 	{
+		GameObject fx = Instantiate(deathFX,gameObject.transform.position,Quaternion.identity);
+		fx.transform.parent=aferdied;
 		print("EM hit"+gameObject.name);
+		Destroy(gameObject);
+		this.deathFX.SetActive(true);
 	}
 }
---
+++ Assets/SelfDestructor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("killSelf",2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void killSelf()
+	{
+		Destroy(gameObject);
+	}	
+}
