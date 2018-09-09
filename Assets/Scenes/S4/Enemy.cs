63ccd08b3d0daad4fd7e912dff43d7048ae9aa37
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-09 19:26:37 +1200

    scroeboard added

--- Assets/Scenes/S4/Enemy.cs
+++ Assets/Scenes/S4/Enemy.cs
@@ -6,17 +6,29 @@ public class Enemy : MonoBehaviour {
 
 	[SerializeField] GameObject deathFX;
 	[SerializeField] Transform  aferdied;
+	ScroreBoard scroreBoard;
 	private void Start()
+	{
+		addcollider(); 
+	}
+
+	private void addcollider()
 	{
 		Collider enemyCollider  = gameObject.AddComponent<SphereCollider>();
 		enemyCollider.isTrigger=false;		
 	}
+
 	private void OnParticleCollision(GameObject other)
 	{
 		GameObject fx = Instantiate(deathFX,gameObject.transform.position,Quaternion.identity);
 		fx.transform.parent=aferdied;
-		print("EM hit"+gameObject.name);
 		Destroy(gameObject);
 		this.deathFX.SetActive(true);
+		updateScoreBoard();
+	}
+	private void updateScoreBoard()
+	{
+		scroreBoard = FindObjectOfType<ScroreBoard>();
+		this.scroreBoard.updateScore();
 	}
 }
---
+++ Assets/Scenes/S4/ScroreBoard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScroreBoard : MonoBehaviour {
+
+	// Use this for initialization
+	private int Score{set;get;}
+	private Text scoreText;
+	void Start () {
+		this.Score=0;
+		this.scoreText= GetComponent<Text>();
+		scoreText.text="Score:" + this.Score;
+	}
+	public void updateScore()
+	{
+		this.Score+=10;
+		this.scoreText.text="Score:" + this.Score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateScore();
+	}
+	
+}
